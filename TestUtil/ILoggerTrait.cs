ff910090c9a57d3af7dc8b39d7991b1f5df79b33
Vadym S. Khondar <vadym@khondar.name>
2020-04-05 19:34:50 +0100

    Commit missing file.

---
+++ TestUtil/ILoggerTrait.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestUtil
+{
+	public interface ILoggerTrait
+	{
+		bool ShouldLog { get; set; }
+
+
+		public void Log(string msg)
+		{
+			if (ShouldLog)
+			{
+				Console.WriteLine($"[{GetType().Name}({GetHashCode()})] {msg}");
+			}
+		}
+	}
+}
