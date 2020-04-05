b87ba02c621d98621fdfa26b08c043bd0a9cbf08
Vadym S. Khondar <vadym@khondar.name>
2020-04-05 19:06:49 +0100

    Fix Main.

--- PureSharp.Console/Program.cs
+++ PureSharp.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using PurseSharp.Lib;
+﻿using System;
+using PurseSharp.Lib;
 using TestUtil;
 
 namespace PureSharp
@@ -9,9 +10,61 @@ namespace PureSharp
 		{
 			var timingAspect = new TimingAspect();
 			timingAspect.Weave<InstrumentedClassPointcut>();
+			
+			var testClass = new TestClass();
+			var instrumentedTestClass = new InstrumentedTestClass();
+			
+			testClass.SuccessfulVoidOperation();
+			instrumentedTestClass.SuccessfulVoidOperation();
+			
+			// try
+			// {
+			// 	instrumentedTestClass.FailedVoidOperation();
+			// }
+			// catch (Exception ex)
+			// {
+			// 	Console.Error.WriteLine($"Caught exception: {ex}");
+			// }
+			
+			var result = testClass.SuccessfulOperationWithReturnValue();
+			result = instrumentedTestClass.SuccessfulOperationWithReturnValue();
+			
+			// try
+			// {
+			// 	result = instrumentedTestClass.FailedOperationWithReturnValue();
+			// }
+			// catch (Exception ex)
+			// {
+			// 	Console.Error.WriteLine($"Caught exception: {ex}");
+			// }
+			
+			Console.WriteLine();
 
-			var instrumentedClass = new InstrumentedClass();
-			instrumentedClass.TimedOperation();
+			testClass.ShouldLog = false;
+			instrumentedTestClass.ShouldLog = false;
+			timingAspect.ShouldLog = false;
+			
+			OverheadTester.Time(
+				"SuccessfulVoidOperation",
+				() => testClass.SuccessfulVoidOperation(),
+				() => instrumentedTestClass.SuccessfulVoidOperation());
+			
+			OverheadTester.Time(
+				"SuccessfulOperationWithReturnValue",
+				() => testClass.SuccessfulOperationWithReturnValue(),
+				() => instrumentedTestClass.SuccessfulOperationWithReturnValue());
+			
+			OverheadTester.Time(
+				"FailedVoidOperation",
+				() => testClass.FailedVoidOperation(),
+				() => instrumentedTestClass.FailedVoidOperation(),
+				100000);
+			
+			OverheadTester.Time(
+				"FailedOperationWithReturnValue",
+				() => testClass.FailedOperationWithReturnValue(),
+				() => instrumentedTestClass.FailedOperationWithReturnValue(),
+				100000);
 		}
 	}
 }
